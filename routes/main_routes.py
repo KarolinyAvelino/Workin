cc76ea11e0521148e3f40ec43d16bba2dbfc6d86
Karoliny Avelino Nascimento Santos <20211IMI037@cefetes.br>
2024-09-06 12:17:48 -0300

    atualizacao

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -11,9 +11,9 @@ templates = obter_jinja_templates("templates/main")
 async def get_root(request: Request):
     return templates.TemplateResponse("pages/index.html", {"request": request})
 
-@router.get("/cadastro", response_class=HTMLResponse)
-async def get_cadastro(request: Request):
-    return templates.TemplateResponse("pages/cadastro.html", {"request": request})
+@router.get("/cadastro_prestador", response_class=HTMLResponse)
+async def get_cadastro_prestador(request: Request):
+    return templates.TemplateResponse("pages/cadastro_prestador.html", {"request": request})
 
 @router.get("/login", response_class=HTMLResponse)
 async def get_login(request: Request):
@@ -31,3 +31,55 @@ async def get_perfil_cliente(request: Request):
 @router.get("/perfil_prestador", response_class=HTMLResponse)
 async def get_perfil_prestador(request: Request):
     return templates.TemplateResponse("pages/perfil_prestador.html", {"request": request})
+
+@router.get("/barra_pesquisa", response_class=HTMLResponse)
+async def get_barra_pesquisa(request: Request):
+    return templates.TemplateResponse("pages/barra_pesquisa.html", {"request": request})
+
+@router.get("/reenviar_codigo", response_class=HTMLResponse)
+async def get_reenviar_codigo(request: Request):
+    return templates.TemplateResponse("pages/reenviar_codigo.html", {"request": request})
+
+@router.get("/cadastro_cliente", response_class=HTMLResponse)
+async def get_cadastro_cliente(request: Request):
+    return templates.TemplateResponse("pages/cadastro_cliente.html", {"request": request})
+
+@router.get("/suporte", response_class=HTMLResponse)
+async def get_suporte(request: Request):
+    return templates.TemplateResponse("pages/suporte.html", {"request": request})
+
+
+@router.get("/dados_cartao", response_class=HTMLResponse)
+async def get_dados_cartao(request: Request):
+    return templates.TemplateResponse("pages/dados_cartao.html", {"request": request})
+
+@router.get("/demanda_cliente", response_class=HTMLResponse)
+async def get_demanda_cliente(request: Request):
+    return templates.TemplateResponse("pages/demanda_cliente.html", {"request": request})
+
+@router.get("/denuncia", response_class=HTMLResponse)
+async def get_denuncia(request: Request):
+    return templates.TemplateResponse("pages/denuncia.html", {"request": request})
+
+@router.get("/demanda_prestador", response_class=HTMLResponse)
+async def get_demanda_prestador(request: Request):
+    return templates.TemplateResponse("pages/demanda_prestador.html", {"request": request})
+
+
+@router.get("/prestador_visao_cliente", response_class=HTMLResponse)
+async def get_prestador_visao_cliente(request: Request):
+    return templates.TemplateResponse("pages/prestador_visao_cliente.html", {"request": request})
+
+
+@router.get("/suporte_adm", response_class=HTMLResponse)
+async def get_suporte_adm(request: Request):
+    return templates.TemplateResponse("pages/suporte_adm.html", {"request": request})
+
+
+@router.get("/comentario_prestador", response_class=HTMLResponse)
+async def get_comentario_prestador(request: Request):
+    return templates.TemplateResponse("pages/comentario_prestador.html", {"request": request})
+
+@router.get("/editar_cliente", response_class=HTMLResponse)
+async def get_editar_cliente(request: Request):
+    return templates.TemplateResponse("pages/editar_cliente.html", {"request": request})
